1bcf72e0d3016bf0e3ec52e9b24a0dad24a40873
qifei <qifei@ovfintech.com>
2018-05-10 13:59:06 +0800

    搜索

--- pages/search/search.js
+++ pages/search/search.js
@@ -11,24 +11,46 @@ Page({
         url: 'http://www.baidu.com',
         price: 100,
         title: '儿童山轮车'
-      },
-      {
-        img: 'http://mmbiz.qpic.cn/mmbiz_jpg/zKZ7UQnGpOFIQrI4tNrlbNLdK7TLicwYCColZoENm2WF2LEhGngkL7OG4vicV47cYKYCgLTau2VZNbX6ic46oEVaw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1',
-        url: 'http://www.baidu.com',
-        price: 90,
-        title: '儿童山轮车'
-      },
-      {
-        img: 'http://mmbiz.qpic.cn/mmbiz_jpg/zKZ7UQnGpOFIQrI4tNrlbNLdK7TLicwYCColZoENm2WF2LEhGngkL7OG4vicV47cYKYCgLTau2VZNbX6ic46oEVaw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1',
-        url: 'http://www.baidu.com',
-        price: 100,
-        title: '儿童山轮车'
       }
     ]
   },
   
+  queryList(product_name) {
+    var _this = this;
+    wx.request({
+      url: 'https://www.isxcxbackend1.cn//bmh_shop/product/info/table', //仅为示例，并非真实的接口地址
+      data: {
+        product_name: product_name,
+        page: _this.data.pageIndex,
+        rows: 10
+      },
+      // header: {
+      //   'content-type': 'application/json' // 默认值
+      // },
+      success: function (res) {
+        if (!res.data.rows || res.data.rows.length < 10) {
+          _this.setData({
+            isEnd: true
+          });
+        } else {
+          _this.setData({
+            isEnd: false
+          });
+        }
+        var data = _this.data.pageIndex === 1 ? res.data.rows : _this.data.listData.concat(res.data.rows);
+        _this.setData({
+          listData: data
+        });
+        cb && cb();
+      }
+    })
+  },
+
   search(e) {
-    wx.showToast({ title: e.detail.value })
+    var product_name = e.detail.value;
+    var that = this;
+    that.queryList(product_name);
+    //wx.showToast({ title: e.detail.value })
   },
 
 
@@ -70,15 +92,32 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh() {
+    var that = this;
+    that.setData({
+      pageIndex: 1
+    });
+    that.queryList(function () {
+      wx.stopPullDownRefresh();
+    });
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom(e) {
+    this.setData({
+      pageIndex: this.data.pageIndex + 1
+    });
+    if (!this.data.isEnd) {
+      this.queryList();
+    } else {
+      wx.showToast({
+        title: '没有更多数据了！',
+        icon: 'none',
+        duration: 2000
+      })
+    }
   },
 
   /**

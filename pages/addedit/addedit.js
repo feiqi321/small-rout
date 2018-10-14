95e1eee2ab1ea6fe91726460efd6b2c2fcb99d9e
齐飞 <qifei@ovfintech.com>
2018-10-14 18:34:21 +0800

    update

--- pages/addedit/addedit.js
+++ pages/addedit/addedit.js
@@ -43,6 +43,7 @@ Page({
       method: 'GET',
       success: function (res) {
         console.log(res)
+<<<<<<< Updated upstream
 
 
         that.setData({
@@ -53,6 +54,19 @@ Page({
 
 
 
+=======
+        that.data.linkman = res.data.data.linkman,
+          that.data.tel = res.data.data.tel,
+          that.data.detail = res.data.data.detail
+        //that.setData({
+        //  linkman = res.data.data.linkman,
+        //  tel = res.data.data.tel,
+        //  detail = res.data.data.detail
+        //})
+        
+     
+        
+>>>>>>> Stashed changes
       }
 
     })
